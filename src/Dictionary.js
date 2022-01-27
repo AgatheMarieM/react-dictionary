f0c5c047df10cd14eb4886d3af428f55b3c4670e
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-27 17:27:00 +0100

    created a new Results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 
 export default function Dictionary() {
     let [query, setQuery] = useState("");
+    let [results, setResults] = useState("");
 
     function handleResponse(response) {
-        console.log(response.data[0])
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
     }
 
     function updateQuery(event) {
@@ -22,9 +25,10 @@ export default function Dictionary() {
     return (
         <div className="Dictionary">
             <form onSubmit={search}>
-                <input type="search" autofocus={true} placeholder="type a word" onChange={updateQuery}/>
+                <input type="search" autoFocus={true} placeholder="type a word" onChange={updateQuery}/>
                 <input type="submit" value="go"/>
             </form>
+            <Results results={results}/>
         </div>
     )
 }
---
+++ src/Results.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Results(props) {
+    console.log(props.results)
+    return (<div className="Results">
+        hello
+    </div>)
+}
