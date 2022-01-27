c2fdf0247c3a7d3446dc9c7fbf3c34cd70b917f7
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-27 15:12:04 +0100

    installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
 
 export default function Dictionary() {
     let [query, setQuery] = useState("");
