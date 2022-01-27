9d7f733af72b27b7a7c8e73e19c3fc85bf552f8b
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-27 15:28:45 +0100

    getting API response for the query

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,13 +6,18 @@ import axios from "axios";
 export default function Dictionary() {
     let [query, setQuery] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0])
+    }
+
     function updateQuery(event) {
       setQuery(event.target.value);      
     }
 
     function search(event){
-        event.preventDefault();             
-        alert(query);  
+        event.preventDefault();          
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
+        axios.get(apiUrl).then(handleResponse);
     }
     return (
         <div className="Dictionary">
