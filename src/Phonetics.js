7baaf02757c385ef8929c2db55e77368f8afd6db
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-02-02 16:33:00 +0100

    updated Phonetics component with Text + Audio link

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,9 +2,12 @@ import React from "react";
 
 
 export default function Phonetics(props) {
+    console.log(props.phonetic);
     return(
         <div className="Phonetics">
-            {props.phonetic.text}
+            <span className="phoneticText">/'{props.phonetic.text}/ </span>
+             {/* might include audio player later*/}
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer" className="phoneticAudio">♪</a>
         </div>
     )
 }
