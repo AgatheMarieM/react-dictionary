d422a3b5f7fea0134d981783839d7dd521abed8f
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-02-02 15:14:31 +0100

    added a loop through all possible definitions on Meanings.js
    meanings can have multiple def

--- src/Meanings.js
+++ src/Meanings.js
@@ -3,14 +3,21 @@ import "./Meanings.css";
 
 export default function Meanings(props) {
     console.log(props.meanings)
+
     return (<div className="Meanings">
         <h3>{props.meanings.partOfSpeech}</h3>
-        <p>
-            {props.meanings.definitions[0].definition}
-        </p> 
-        <p>
-            <em>"{props.meanings.definitions[0].example}"</em>
-        </p>        
-    </div>
-    )
+        {/* mapping through all the definitions(some words have more than 1)*/}
+        {props.meanings.definitions.map(function(definition, index){
+            return(
+                <div key={index}>
+                <p>
+                    {definition.definition}
+                <br />
+                <em>"{definition.example}"</em>
+                </p>
+                </div>
+            )
+        })}
+        </div>
+    );
 }
