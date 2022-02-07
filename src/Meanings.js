e253b5fc78a3f043b56adbef9b433127f3dc7ea1
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-02-08 00:10:20 +0100

    aligned examples with definition, removed brackets as some definitions avec no examples

--- src/Meanings.js
+++ src/Meanings.js
@@ -3,22 +3,19 @@ import "./Meanings.css";
 import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
-        return (<div className="Meanings">
-        <h3>{props.meanings.partOfSpeech}</h3>
-        {/* mapping through all the definitions(some words have more than 1)*/}
+        return (
+        <div className="Meanings">
+        <h3>{props.meanings.partOfSpeech}</h3>        
         {props.meanings.definitions.map(function(definition, index){
             return(
                 <div key={index}> 
-                <div className="definitions">             
+                <span className="definitions">             
                     <span className="definitionTitle">Definition</span>
-                    <span className="definitionText">{definition.definition}</span>                    
-                </div>  
-                <div className="examples"> 
-                
-                <span className="examplesText"><em>"{definition.example}"</em></span>
-                </div>   
-                <Synonyms synonyms={definition.synonyms} />                
-                </div>
+                    <span className="definitionText">{definition.definition}</span>    
+                </span>           
+                    <span className="examplesText"><em>ex: {definition.example}</em></span>             
+                    <Synonyms synonyms={definition.synonyms} />                
+                </div>                
             )
         })}
         </div>
