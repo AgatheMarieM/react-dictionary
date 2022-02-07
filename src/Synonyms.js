eb58851d63d0d0e39d15d67b51ef7c6900213c62
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-02-07 23:02:12 +0100

    updated css (margins, colors)

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {  
-        if (props.synonyms) {
-          return(
-              <div className="Synonyms">
-                  <span className="synonymsTitle">Synonyms</span>
-                  <ul className="Synonyms">
-                  {/* mapping through all synonyms to split the synonyms array*/}
-           {props.synonyms.map(function(synonym, index) {
-               return(
-                   <li key={index}><span>{synonym}</span></li>                   
-               )
-           })}
-                 </ul>
-            </div>
-              );  
-        } else {
-            return null;
-        }        
+    if (props.synonyms) {
+      return(
+          <div className="Synonyms">
+              <span className="synonymsTitle">Synonyms</span>
+              <ul className="Synonyms">
+              {/* mapping through all synonyms to split the synonyms array*/}
+       {props.synonyms.map(function(synonym, index) {
+           return(
+               <li key={index}><span>{synonym}</span></li>                   
+           )
+       })}
+             </ul>
+        </div>
+          );  
+    } else {
+        return null;
+    }        
 }
