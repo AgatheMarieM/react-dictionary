a349d35da94cbf2486bcdbe416b908e418160a71
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-02-07 23:04:25 +0100

    working on not returning "Synonyms" title if there are no synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./Synonyms.css";
 
-export default function Synonyms(props) {  
-    if (props.synonyms) {
-      return(
-          <div className="Synonyms">
-              <span className="synonymsTitle">Synonyms</span>
-              <ul className="Synonyms">
-              {/* mapping through all synonyms to split the synonyms array*/}
-       {props.synonyms.map(function(synonym, index) {
-           return(
-               <li key={index}><span>{synonym}</span></li>                   
-           )
-       })}
-             </ul>
-        </div>
-          );  
-    } else {
-        return null;
-    }        
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <span className="synonymsTitle">Synonyms</span>
+        <ul className="Synonyms">
+          {/* mapping through all synonyms to split the synonyms array*/}
+          {props.synonyms.map(function (synonym, index) {
+            if (synonym.length > 0) {
+              return (
+                <li key={index}>
+                  <span>{synonym}</span>
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
