3ee223e065ac90102a82de3c02bf1bcf830c8779
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-02-07 23:27:33 +0100

    updated Synonyms.js : added an if condition

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+    console.log(props.synonyms);
+  if (props.synonyms.length > 0) {
     return (
       <div className="Synonyms">
         <span className="synonymsTitle">Synonyms</span>
         <ul className="Synonyms">
           {/* mapping through all synonyms to split the synonyms array*/}
           {props.synonyms.map(function (synonym, index) {
-            if (synonym.length > 0) {
+            if (synonym) {
               return (
                 <li key={index}>
                   <span>{synonym}</span>
